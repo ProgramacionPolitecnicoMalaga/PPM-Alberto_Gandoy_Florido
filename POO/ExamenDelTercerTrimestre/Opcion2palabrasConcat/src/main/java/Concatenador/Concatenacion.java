018aaaaf47b32d47f4a109cb510fe18aef48298a
Albertogf98 <alberto1998adoratrices@gmail.com>
2020-05-21 17:32:15 +0200

    SUBIDA

--- POO/ExamenDelTercerTrimestre/Opcion2palabrasConcat/src/main/java/Concatenador/Concatenacion.java
+++ POO/ExamenDelTercerTrimestre/Opcion2palabrasConcat/src/main/java/Concatenador/Concatenacion.java
@@ -1,53 +1,25 @@
 package Concatenador;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Concatenacion {
-    private List<String> palabras;
 
-    public Concatenacion() {
-        this.palabras = new ArrayList<>();
-    }
-
-    public List<String> getPalabras() {
-        return palabras;
-    }
-
-    public void setPalabras(List<String> palabras) {
-        this.palabras = palabras;
-    }
-
-    public void addPalabra(String palabra) {
-        if (palabra == null)
-            throw new IllegalArgumentException("La palabra es nula");
-        else palabras.add(palabra);
-    }
-
-    public List<String> concatenar(List<String> palabras) {
-        String cadena = "";
+    public String concatenador (List<String> palabras) {
+        String concatenada = "";
         for (int i = 0; i < palabras.size(); i++) {
-            cadena += palabras.get(i);
+            concatenada += palabras.get(i);
         }
-        this.palabras.add(cadena);
-        return this.palabras;
+        return concatenada;
     }
 
-    @Override
-    public String toString() {
-        return "Concatenar{" +
-                "palabras=" + palabras +
-                '}';
-    }
 
-   /* public static void main(String[] args) {
-        Concatenar concatenar =new Concatenar();
+    public static void main(String[] args) {
         ArrayList<String> palabra = new ArrayList<>();
+        palabra.add("fdfgdsf");
         palabra.add("HOLA");
-        palabra.add("DDDD");
-        palabra.add("CCCCC");
-        System.out.println(concatenar.concatenar(palabra));
+        Concatenacion concatenacion = new Concatenacion();
+        System.out.println(concatenacion.concatenador(palabra));
 
-    }*/
+    }
 }
